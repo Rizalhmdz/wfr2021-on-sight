18e706ecbdbb05b6393f6fcf9ed20f4ca86778bc
Rizalmhmmd13zx <rizalmhmmd.13zx@gmail.com>
2021-10-09 13:18:12 +0800

    menambahkan view login, signup dan home

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/signup', function () {
+    return view('signup');
+});
+
+Route::get('/admin', function () {
+    return view('admin');
 });
